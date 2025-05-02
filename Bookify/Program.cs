3b8cb2b5faadb078a5420fa8472fe622a0355831
AHMEDSAEEDGIT <a.seed5513@gmail.com>
2025-05-02 12:30:55 +0300

    full CRUD operation on cateogy entity

--- Bookify/Program.cs
+++ Bookify/Program.cs
@@ -1,3 +1,6 @@
+using Bookify.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace Bookify
 {
     public class Program
@@ -8,6 +11,10 @@ namespace Bookify
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+            });
 
             var app = builder.Build();
 
